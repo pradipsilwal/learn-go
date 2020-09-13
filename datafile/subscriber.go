3a4d61da5c2e6826ee77a0611906180e78f59683
Pradip Silwal <silwal.pradip@outlook.com>
2020-09-13 22:47:27 +1000

    embeded struct

--- datafile/subscriber.go
+++ datafile/subscriber.go
@@ -1,8 +1,30 @@
 package datafile
 
-//Subscriber type for exporting
+//Subscriber information
 type Subscriber struct {
 	Name   string
 	Rate   float64
 	Active bool
+	// HomeAddress Address
+
+	//Anonymous field
+	Address
+}
+
+//Employee information
+type Employee struct {
+	Name   string
+	Salary float64
+	// HomeAddress Address
+
+	//Anonymous field
+	Address
+}
+
+//Address information
+type Address struct {
+	Street     string
+	City       string
+	State      string
+	PostalCode string
 }
--- importingStruct.go
+++ importingStruct.go
@@ -7,7 +7,14 @@ import (
 
 func importingStruct() {
 	subscriber1 := datafile.Subscriber{Name: "Pradip Silwal", Rate: 5.5, Active: true}
+	address1 := datafile.Address{Street: "3 Gladstone Parade", City: "Glenroy", State: "VIC", PostalCode: "3046"}
+	subscriber1.Address = address1
 	fmt.Println("Name: ", subscriber1.Name)
 	fmt.Println("Rate: ", subscriber1.Rate)
 	fmt.Println("Active? ", subscriber1.Active)
+	// fmt.Println("Home Address: ", subscriber1.Address)
+
+	//Since we are using anonymous field in the struct we can
+	//access the embeded struct variable using the outer structs
+	fmt.Println("City: ", subscriber1.City)
 }
