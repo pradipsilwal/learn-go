b36dbba884b7bc73b9a881c5fca3e3d2bc196a36
Pradip Silwal <silwal.pradip@outlook.com>
2020-09-17 20:37:27 +1000

    interfaces

--- tapeInterface.go
+++ tapeInterface.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"gadget"
+)
+
+type Player interface {
+	Play(string)
+	Stop()
+}
+
+func playList(device Player, songs []string) {
+	for _, song := range songs {
+		device.Play(song)
+	}
+	device.Stop()
+}
+
+func tryOut(player Player, song string) {
+	player.Play(song)
+	player.Stop()
+	recorder, ok := player.(gadget.TapeRecorder)
+	if ok {
+		recorder.Record()
+	}
+}
 
 func tapeInterface() {
-	fmt.Println("Hello")
+	var player Player = gadget.TapeRecorder{}
+	tryOut(player, "Shake it off")
+	player = gadget.TapePlayer{}
+	tryOut(player, "Delicate")
 }
